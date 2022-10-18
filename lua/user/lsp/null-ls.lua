157e9674de6d0242312edc690055fc18acc46d74
Fengzi21 <iwtbags@163.com>
2022-10-18 10:39:23 +0800

    set --line-length=100 for black formatter

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -7,16 +7,30 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/completion
+local completion = null_ls.builtins.completion
 
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
 null_ls.setup {
   debug = false,
   sources = {
+    completion.spell,
     formatting.prettier.with {
       extra_filetypes = { "toml" },
       extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     },
-    formatting.black.with { extra_args = { "--fast" } },
+    -- formatting.black.with {
+    --   extra_args = {
+    --     "--fast",
+    --     "--line-length=100",
+    --   },
+    -- },
+    formatting.blue.with {
+      extra_args = {
+        "--fast",
+        "--line-length=100",
+      },
+    },
     formatting.stylua,
     formatting.google_java_format,
     formatting.rustfmt,
