07e011199544a40907ba451a241c0305983dc5cf
Fengzi21 <iwtbags@163.com>
2022-10-19 21:29:18 +0800

    add winbar on toggleterm

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -1,39 +1,51 @@
 local status_ok, toggleterm = pcall(require, "toggleterm")
 if not status_ok then
-	return
+  return
 end
 
+local fmt = string.format
+
 toggleterm.setup({
-	size = 20,
-	open_mapping = [[<c-\>]],
-	hide_numbers = true,
+  -- size can be a number or function which is passed the current terminal
+  size = 20,
+  open_mapping = [[<c-\>]],
+  hide_numbers = true, -- hide the number column in toggleterm buffers
   shade_filetypes = {},
-	shade_terminals = true,
-	shading_factor = 2,
-	start_in_insert = true,
-	insert_mappings = true,
-	persist_size = true,
-	direction = "float",
-	close_on_exit = true,
-	shell = vim.o.shell,
-	float_opts = {
-		border = "curved",
+  shade_terminals = true,
+  shading_factor = 2,
+  start_in_insert = true,
+  insert_mappings = true, -- whether or not the open mapping applies in insert model
+  terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+  persist_size = true,
+  -- direction = "float",
+  direction = "horizontal",
+  close_on_exit = true,
+  shell = vim.o.shell,
+  -- This field is only relevant if direction is set to 'float'
+  float_opts = {
+    border = "curved",
     winblend = 0,
     highlights = {
       border = "Normal",
       background = "Normal",
-    }
-	},
+    },
+  },
+  winbar = {
+    enabled = true,
+    name_formatter = function (term)
+      return fmt("%d:%s", term.id, term:_display_name())
+    end
+  },
 })
 
 function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
+  local opts = { noremap = true }
   vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<jk>', [[<C-\><C-n>]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
@@ -42,25 +54,25 @@ local Terminal = require("toggleterm.terminal").Terminal
 
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 function _LAZYGIT_TOGGLE()
-	lazygit:toggle()
+  lazygit:toggle()
 end
 
 local node = Terminal:new({ cmd = "node", hidden = true })
 function _NODE_TOGGLE()
-	node:toggle()
+  node:toggle()
 end
 
 local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
 function _NCDU_TOGGLE()
-	ncdu:toggle()
+  ncdu:toggle()
 end
 
 local htop = Terminal:new({ cmd = "htop", hidden = true })
 function _HTOP_TOGGLE()
-	htop:toggle()
+  htop:toggle()
 end
 
 local python = Terminal:new({ cmd = "python", hidden = true })
 function _PYTHON_TOGGLE()
-	python:toggle()
+  python:toggle()
 end
