a58de322455ee5b4a9a53d9851914e2b7a88b662
Fengzi21 <iwtbags@163.com>
2022-10-23 17:42:11 +0800

    nonm italic

--- lua/user/monokaipro.lua
+++ lua/user/monokaipro.lua
@@ -3,7 +3,9 @@
 
 -- Example config in Lua
 vim.g.monokaipro_filter = "classic"  -- "machine"
-vim.g.monokaipro_italic_functions = true
+vim.g.monokaipro_italic_comments = false
+vim.g.monokaipro_italic_keywords = false
+vim.g.monokaipro_italic_functions = false
 vim.g.monokaipro_sidebars = { "vista_kind", "packer" }
 vim.g.monokaipro_flat_term = true
 
