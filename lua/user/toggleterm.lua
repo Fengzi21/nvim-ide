937b7f4e437da83f7b055331019b8e15387b7099
Fengzi21 <iwtbags@163.com>
2022-12-24 21:42:27 +0800

    toggleterm works for powershell

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -6,6 +6,19 @@ end
 
 local fmt = string.format
 
+local powershell_options = {
+  shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  shellquote = "",
+  shellxquote = "",
+}
+
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
+
 local conf = {
   -- size can be a number or function which is passed the current terminal
   size = function (term)
