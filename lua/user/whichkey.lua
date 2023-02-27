420d3cd969d1bce2d18e355332404f87783430f8
Fengzi21 <iwtbags@163.com>
2023-02-27 12:32:24 +0800

    add lazygit keymapping

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -217,6 +217,7 @@ local mappings = {
 
   t = {
     name = "Terminal",
+    g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
     n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
     u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
     t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
