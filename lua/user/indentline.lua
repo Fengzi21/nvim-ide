f5012d0113b7050706dbf4c284e7d9bea53761b4
Fengzi21 <iwtbags@163.com>
2023-10-31 21:53:40 +0800

    migrate indent-blankline to version 3

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -1,18 +1,24 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, ibl = pcall(require, "ibl")
 if not status_ok then
   return
 end
 
-indent_blankline.setup {
-  char = "▏",
-  show_trailing_blankline_indent = false,
-  show_first_indent_level = true,
-  use_treesitter = true,
-  show_current_context = true,
-  buftype_exclude = { "terminal", "nofile" },
-  filetype_exclude = {
-    "help",
-    "packer",
-    "NvimTree",
+ibl.setup {
+  indent = {
+    char = "▏",
   },
+  whitespace = {
+    remove_blankline_trail = true,
+  },
+  exclude = {
+    filetypes = {
+      "help",
+      "packer",
+      "NvimTree",
+    },
+    buftypes = {
+      "terminal",
+      "nofile"
+    },
+  }
 }
