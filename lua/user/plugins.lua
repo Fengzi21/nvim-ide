028a7e7357ed1d7ae6c378f0de436f32cd0d2a37
Fengzi21 <iwtbags@163.com>
2024-03-29 21:29:06 +0800

    add mojo.vim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -159,6 +159,9 @@ return packer.startup(function(use)
   -- for LaTeX
   use { 'lervag/vimtex' }
 
+  -- for Mojo
+  use { "igorgue/mojo.vim" }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
