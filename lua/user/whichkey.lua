f04992c877ac364cf35e85c4656bb388d59b7bf1
Fengzi21 <iwtbags@163.com>
2024-11-12 21:19:50 +0800

    add Run Buffer

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -5,22 +5,22 @@ end
 
 local setup = {
   plugins = {
-    marks = true,       -- shows a list of your marks on ' and `
-    registers = true,   -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+    marks = true, -- shows a list of your marks on ' and `
+    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
     spelling = {
-      enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+      enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
       suggestions = 20, -- how many suggestions should be shown in the list?
     },
     -- the presets plugin, adds help for a bunch of default keybindings in Neovim
     -- No actual key bindings are created
     presets = {
-      operators = false,    -- adds help for operators like d, y, ... and registers them for motion / text object completion
-      motions = false,      -- adds help for motions
+      operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+      motions = false, -- adds help for motions
       text_objects = false, -- help for text objects triggered after entering an operator
-      windows = true,       -- default bindings on <c-w>
-      nav = true,           -- misc bindings to work with windows
-      z = true,             -- bindings for folds, spelling and others prefixed with z
-      g = true,             -- bindings for prefixed with g
+      windows = true, -- default bindings on <c-w>
+      nav = true, -- misc bindings to work with windows
+      z = true, -- bindings for folds, spelling and others prefixed with z
+      g = true, -- bindings for prefixed with g
     },
   },
   -- add operators that will trigger motion and text object completion
@@ -52,8 +52,8 @@ local setup = {
   layout = {
     height = { min = 4, max = 25 }, -- min and max height of the columns
     width = { min = 20, max = 50 }, -- min and max width of the columns
-    spacing = 3,                    -- spacing between columns
-    align = "left",                 -- align columns left, center or right
+    spacing = 3, -- spacing between columns
+    align = "left", -- align columns left, center or right
   },
   -- ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
   -- hidden = { "<silent>", ":", "<CMD>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
@@ -72,10 +72,10 @@ local setup = {
 local opts = {
   mode = { "n", "v", "x" },
   prefix = "<leader>",
-  buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true,  -- use `silent` when creating keymaps
+  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true, -- use `silent` when creating keymaps
   noremap = true, -- use `noremap` when creating keymaps
-  nowait = true,  -- use `nowait` when creating keymaps
+  nowait = true, -- use `nowait` when creating keymaps
 }
 
 local mappings = {
@@ -237,11 +237,15 @@ local mappings = {
     },
   },
 
-  R = {
-    name = "Run",
-    p = { "::w<CR>:exec '!python3' shellescape(@%, 1)<CR>", "Python" },
-    l = { "::w<CR>:exec '!lua' shellescape(@%, 1)<CR>", "Lua" },
-  },
+  -- R = {
+  --   name = "Run",
+  --   p = { "::w<CR>:exec '!python3' shellescape(@%, 1)<CR>", "Python" },
+  --   l = { "::w<CR>:exec '!lua' shellescape(@%, 1)<CR>", "Lua" },
+  --   r = { "::w<CR>:exec '!run' shellescape(@%, 1)<CR>", "Run Buffer" },
+  -- },
+  --
+
+  R = { "::w<CR>:exec '!run' shellescape(@%, 1)<CR>", "Run Buffer" },
 
   s = {
     name = "Search",
