32041cb42dede396762aef64cc5bf5e9dd1eb031
Fengzi21 <iwtbags@163.com>
2024-12-30 09:47:29 +0800

    add markdown config

---
+++ lua/user/markdown.lua
@@ -0,0 +1,12 @@
+-- markdown-preview --
+
+-- set to 1, the nvim will auto close current preview window when changing
+-- from Markdown buffer to another buffer
+-- default: 1
+vim.g.mkdp_auto_close = 0
+
+-- use custom IP to open preview page.
+-- Useful when you work in remote Vim and preview on local browser.
+-- For more details see: https://github.com/iamcco/markdown-preview.nvim/pull/9
+-- default empty
+vim.g.mkdp_open_ip = '10.201.113.4'
