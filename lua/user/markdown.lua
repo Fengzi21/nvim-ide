d3b8066355ab1a2f75c12d9bcb1b8fcab63471ef
Fengzi21 <iwtbags@163.com>
2024-12-30 10:00:52 +0800

    small update

--- lua/user/markdown.lua
+++ lua/user/markdown.lua
@@ -5,8 +5,15 @@
 -- default: 1
 vim.g.mkdp_auto_close = 0
 
+-- set to 1, the preview server is available to others in your network.
+-- By default, the server listens on localhost (127.0.0.1)
+-- default: 0
+-- vim.g.mkdp_open_to_the_world = 1
+
 -- use custom IP to open preview page.
 -- Useful when you work in remote Vim and preview on local browser.
 -- For more details see: https://github.com/iamcco/markdown-preview.nvim/pull/9
 -- default empty
-vim.g.mkdp_open_ip = '10.201.113.4'
+-- vim.g.mkdp_open_ip = '10.201.113.4'
+
+-- vim.g.mkdp_echo_preview_url = 1
