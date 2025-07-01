bac686a6c6109eeea891c890575698510e8eb1cd
Fengzi21 <iwtbags@163.com>
2025-07-01 16:34:50 +0800

    disable semshi

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -53,7 +53,7 @@ packer.init({
 return packer.startup(function(use)
   -- My plugins here --
   use({ "wbthomason/packer.nvim" }) -- Have packer manage itself
-  use({ "nvim-lua/plenary.nvim" }) -- Useful lua functions used by lots of plugins
+  use({ "nvim-lua/plenary.nvim" })  -- Useful lua functions used by lots of plugins
   use({ "numToStr/Comment.nvim" })
   use({ "JoosepAlviste/nvim-ts-context-commentstring" })
   use({ "nvim-tree/nvim-web-devicons" })
@@ -76,8 +76,8 @@ return packer.startup(function(use)
 
   -- Colorschemes --
   use({ "folke/tokyonight.nvim" })
-  use({ "numirias/semshi", run = ":UpdateRemotePlugins" })
   use({ "tanvirtin/monokai.nvim" })
+  -- use({ "Fengzi21/semshi", run = ":UpdateRemotePlugins" })
   use({ "vim-python/python-syntax" })
 
   -- colorizer --
@@ -90,9 +90,9 @@ return packer.startup(function(use)
   use({ "xiyaowong/nvim-transparent" })
 
   -- cmp plugins for completion --
-  use({ "hrsh7th/nvim-cmp" }) -- The completion plugin
-  use({ "hrsh7th/cmp-buffer" }) -- buffer completions
-  use({ "hrsh7th/cmp-path" }) -- path completions
+  use({ "hrsh7th/nvim-cmp" })         -- The completion plugin
+  use({ "hrsh7th/cmp-buffer" })       -- buffer completions
+  use({ "hrsh7th/cmp-path" })         -- path completions
   use({ "saadparwaiz1/cmp_luasnip" }) -- snippet completions
   use({ "hrsh7th/cmp-nvim-lsp" })
   use({ "hrsh7th/cmp-nvim-lua" })
@@ -104,12 +104,13 @@ return packer.startup(function(use)
     dependencies = { "rafamadriz/friendly-snippets" },
     requires = { "rafamadriz/friendly-snippets" },
     build = "make install_jsregexp",
-  }) -- snippet engine
+  })                                      -- snippet engine
   use({ "rafamadriz/friendly-snippets" }) -- a bunch of snippets to use
 
   -- LSP --
-  use({ "neovim/nvim-lspconfig" }) -- enable LSP
+  use({ "neovim/nvim-lspconfig" })           -- enable LSP
   use({ "williamboman/nvim-lsp-installer" }) -- simple to use language server installer
+  use { 'neoclide/coc.nvim', branch = 'release' }
 
   -- null-ls has beem archived and will no longer receive updates
   -- use({ "jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
