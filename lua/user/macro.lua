f54dd6e9183f34d22f6f99e753f2ba6a90c2b91d
Fengzi21 <iwtbags@163.com>
2025-07-08 12:18:52 +0800

    add some latex macro

--- lua/user/macro.lua
+++ lua/user/macro.lua
@@ -1,5 +1,15 @@
--- 宏 b: 加粗 latex 文本
+-- 宏 b: \textbf 加粗 latex 文本
 vim.fn.setreg("b", "xi\\textbf{\027pa}\027")
 
--- 宏 h: 自定义命令高亮 latex 文本
+-- 宏 m: \bm 加粗 latex 公式
+vim.fn.setreg("m", "xi\\bm{\027pa}\027")
+
+-- 宏 h: \highlight 高亮 latex 文本
+-- latex 自定义蓝色高亮: \newcommand{\highlight}[1]{\textcolor{blue}{#1}}
 vim.fn.setreg("h", "xi\\highlight{\027pa}\027")
+
+-- 宏 i: turn selected to inline math
+vim.fn.setreg("i", "xi\\(\027pa\\)\027")
+
+-- 宏 d: turn selected to display math
+vim.fn.setreg("d", "xi\\[\027pa\\]\027")
